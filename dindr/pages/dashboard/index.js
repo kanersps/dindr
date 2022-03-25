b2a2ec4989180fe6ce0d73535913c5d4a0ec8457
Jasper123Pyah <jaspermeiracker@gmail.com>
2022-03-25 12:51:59 +0100

    decentralized

--- dindr/pages/dashboard/index.js
+++ dindr/pages/dashboard/index.js
@@ -1,7 +1,7 @@
+export default function Dashboard() {
+    const GetStack = () => {
 
-
-export default function Dashboard(){
-
+    }
     return <div>
 
     </div>
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,8 @@
+require("@nomiclabs/hardhat-waffle");
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-
-require("dotenv").config();
-require("@nomiclabs/hardhat-ethers");
-
-const { API_URL, PRIVATE_KEY } = process.env;
-
 module.exports = {
-  solidity: "0.8.9",
-  defaultNetwork: "polygon_mumbai",
-  networks: {
-    hardhat: {},
-    polygon_mumbai: {
-      url: API_URL,
-      accounts: [`0x${PRIVATE_KEY}`],
-    },
-  },
+  solidity: "0.8.4",
 };
